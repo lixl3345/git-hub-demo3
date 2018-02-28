987725722e0e051ad49ca73ff1173c7f63c30095
lxl <lxl@lxl-PC>
2018-02-28 11:17:18 +0800

    dddd

---
+++ src/main/java/lixianglin/com/git_hub_demo3/App.java
@@ -0,0 +1,17 @@
+package lixianglin.com.git_hub_demo3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!333
+ *
+ */
+@SpringBootApplication
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	SpringApplication.run(App.class, args);
+    }
+}
